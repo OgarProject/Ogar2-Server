efabfd4c074c3146e3006f529a8b0ee9d007020a
Romarjosh Villamor <thundergemios10@gmail.com>
2016-03-25 10:01:39 -0700

    Move to a package and add package declaration.

--- src/main/java/com/ogarproject/ogar/server/config/Configuration.java
+++ src/main/java/com/ogarproject/ogar/server/config/Configuration.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Ogar.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+package com.ogarproject.ogar.server.config;
 
 import java.io.FileReader;
 import java.util.Properties;
